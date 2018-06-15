4678cc8c1c344a41590d1f23b0ceebe50ba904b3
fernandohfs94 <fernando.hfs94@gmail.com>
2018-06-15 17:49:19 -0300

    Resolve os exercícios match_ends e front_x

--- basic/list1.py
+++ basic/list1.py
@@ -22,8 +22,14 @@
 # and last chars of the string are the same.
 # Note: python does not have a ++ operator, but += works.
 def match_ends(words):
-    # +++your code here+++
-    return
+    count = 0
+
+    for word in words:
+        if len(word) >= 2:
+            if word[0] == word[-1]:
+                count += 1
+
+    return count
 
 
 # B. front_x
@@ -34,8 +40,19 @@ def match_ends(words):
 # Hint: this can be done by making 2 lists and sorting each of them
 # before combining them.
 def front_x(words):
-    # +++your code here+++
-    return
+    list_x = []
+    remaining_list = []
+
+    for word in words:
+        if word[0] == 'x':
+            list_x.append(word)
+        else:
+            remaining_list.append(word)
+
+    list_x = sorted(list_x)
+    remaining_list = sorted(remaining_list)
+
+    return list_x + remaining_list
 
 
 # C. sort_last
